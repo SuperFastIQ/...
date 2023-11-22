93f65087e9f58c642a4d4070be754da37cd12383
SuperFastIQ <SuperFastIQ@gmail.co>
2023-11-22 17:02:06 +0100

    Working finish for levels

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,27 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
     private AudioSource _finishSound;
+
+    private bool _finishedLevel = false;
 
     private void Start()
     {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
         _finishSound = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player")
+        if(collision.gameObject.name == "Player" && !_finishedLevel)
         {
             _finishSound.Play();
-            CompleteLevel();
+            _finishedLevel = true;
+            Invoke("CompleteLevel", 2f);
+            _rigidbody2D.bodyType = RigidbodyType2D.Static;
         }
     }
 
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
